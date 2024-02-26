7c6c02eafc11bcf006422539b5e54f23eab315e4
wbc <919006380@qq.com>
2024-02-26 18:27:54 +0800

    add

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.reverseString = void 0;
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+exports.reverseString = reverseString;
